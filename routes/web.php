560aa83de2932a040a60e319d72cef3380409104
MohamedHamouch <med10hamouch@gmail.com>
2025-03-13 02:34:20 +0000

    create controller and view pages files

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Cache;
-
+use App\Http\Controllers\CBController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
 
-
-Route::get('/test-redis-cache', function () {
-    
     // Store a value in the cache
-    Cache::put('test_key', 'test_value', 0.5); // Cache for 10 minutes
+    // Cache for 10 seconds
+    Cache::put('test_key', 'test_value', 10);
 
     // Retrieve the value from the cache
     $value = Cache::get('test_key');
 
-    return $value; // Should return 'test_value'
+    return $value;
 });
+
+Route::prefix('users')->name('users.')->group(function () {
+    Route::get('/', [CBController::class, 'index'])->name('index');
+    Route::get('/create', [CBController::class, 'create'])->name('create');
+    Route::post('/', [CBController::class, 'store'])->name('store');
+});
+
